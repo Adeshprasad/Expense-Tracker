dfc579ba3428bb3075f4a2764414b159bb0eb9b4
Adesh Prasad <adeshprasad010@gmail.com>
2026-01-31 20:19:33 +0530

    Configured MySQL and JPA entities

--- src/main/java/com/adesh/expense_tracker/ExpenseTrackerApplication.java
+++ src/main/java/com/adesh/expense_tracker/ExpenseTrackerApplication.java
@@ -2,12 +2,13 @@ package com.adesh.expense_tracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.adesh.expense_tracker.model")
 public class ExpenseTrackerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ExpenseTrackerApplication.class, args);
 	}
-
 }
